bdd2e6d3d4c33706e6839d6c78dfa6d2f8c3ca40
SueXY <SuXYMC@163.com>
2025-02-04 20:23:25 +0800

    Save center in evo, format files

--- demo.py
+++ demo.py
@@ -1,7 +1,8 @@
-'''
+"""
 demonstrate trained network
 -h for help
-'''
+"""
+
 import argparse
 import torch
 import gymnasium as gym
@@ -9,10 +10,8 @@ from time import sleep
 import Game2048Env
 import pygame
 
-device = torch.device(
-    "cuda" if torch.cuda.is_available() else
-    "cpu"
-)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 
 def wait_for_key():
     while True:
@@ -20,26 +19,40 @@ def wait_for_key():
         if event.type == pygame.KEYDOWN:
             return
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='evaluate trained model for 2048 game')
-    parser.add_argument('path', type=str, help='path to trained model')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="evaluate trained model for 2048 game")
+    parser.add_argument("path", type=str, help="path to trained model")
     parser.add_argument(
-        '-r',
-        '--render-mode',
+        "-r",
+        "--render-mode",
         type=str,
-        default='human',
-        choices=['human', 'ansi'],
-        help='render mode for demo'
+        default="human",
+        choices=["human", "ansi"],
+        help="render mode for demo",
     )
-    parser.add_argument('-d', '--delay', type=float, default=0.5, help='delay between steps, ignored if -p is on')
-    parser.add_argument('-p', '--pause', action='store_true', help='pause and wait for keypress after each step')
+    parser.add_argument(
+        "-d",
+        "--delay",
+        type=float,
+        default=0.5,
+        help="delay between steps, ignored if -p is on",
+    )
+    parser.add_argument(
+        "-p",
+        "--pause",
+        action="store_true",
+        help="pause and wait for keypress after each step",
+    )
     args = parser.parse_args()
     model = torch.load(args.path)
     model.eval()
 
-    env = gym.make('Game2048Env/Game2048-v0', render_mode=args.render_mode)
+    env = gym.make("Game2048Env/Game2048-v0", render_mode=args.render_mode)
     state, _ = env.reset()
-    state = torch.tensor(state.flatten(), dtype=torch.float32, device=device).unsqueeze(0)
+    state = torch.tensor(state.flatten(), dtype=torch.float32, device=device).unsqueeze(
+        0
+    )
 
     while True:
         with torch.no_grad():
@@ -48,13 +61,11 @@ if __name__ == '__main__':
         if terminated or truncated:
             break
         state = torch.tensor(
-            observation.flatten(),
-            dtype=torch.float32,
-            device=device
+            observation.flatten(), dtype=torch.float32, device=device
         ).unsqueeze(0)
-        if args.render_mode == 'ansi':
+        if args.render_mode == "ansi":
             print(env.render())
-        if args.render_mode == 'human' and args.pause:
+        if args.render_mode == "human" and args.pause:
             wait_for_key()
         else:
             sleep(args.delay)
--- trainevo.py
+++ trainevo.py
@@ -1,7 +1,8 @@
-'''
+"""
 use evolutionary algorithm to train
 -h for help
-'''
+"""
+
 import argparse
 import torch
 import evotorch
@@ -11,24 +12,49 @@ from evalmodel import evaluate_model
 import matplotlib.pyplot as plt
 from network import *
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='train network using evo for 2048 game')
-    parser.add_argument('template_path', type=str, help='path to network template')
-    parser.add_argument('save_path', type=str, help='path to save the trained network')
-    parser.add_argument('episodes', type=int, help='episodes to train the network')
-    parser.add_argument('--popsize', type=int, default=64, help='population size of the algorithm')
-    parser.add_argument('--radius_init', type=float, default=2.25, help='initial radius of the algorithm')
-    parser.add_argument('--center-learning-rate', type=float, default=0.2, help='learning rate of the center of the algorithm')
-    parser.add_argument('--stdev-learning-rate', type=float, default=0.1, help='learning rate of the stdev of the algorithm')
-    parser.add_argument('-i', '--interval', type=int, default=16, help='interval of logging, no log if 0')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="train network using evo for 2048 game"
+    )
+    parser.add_argument("template_path", type=str, help="path to network template")
+    parser.add_argument("save_path", type=str, help="path to save the trained network")
+    parser.add_argument("episodes", type=int, help="episodes to train the network")
+    parser.add_argument(
+        "--popsize", type=int, default=64, help="population size of the algorithm"
+    )
+    parser.add_argument(
+        "--radius_init",
+        type=float,
+        default=2.25,
+        help="initial radius of the algorithm",
+    )
+    parser.add_argument(
+        "--center-learning-rate",
+        type=float,
+        default=0.2,
+        help="learning rate of the center of the algorithm",
+    )
+    parser.add_argument(
+        "--stdev-learning-rate",
+        type=float,
+        default=0.1,
+        help="learning rate of the stdev of the algorithm",
+    )
+    parser.add_argument(
+        "-i",
+        "--interval",
+        type=int,
+        default=16,
+        help="interval of logging, no log if 0",
+    )
     args = parser.parse_args()
 
     problem = evotorch.neuroevolution.NEProblem(
         "max",
         network=torch.load(args.template_path),
         network_eval_func=evaluate_model,
-        num_actors='max',
-        num_gpus_per_actor = 'max'
+        num_actors="max",
+        num_gpus_per_actor="max",
     )
 
     searcher = evotorch.algorithms.PGPE(
@@ -36,7 +62,7 @@ if __name__ == '__main__':
         popsize=args.popsize,
         radius_init=args.radius_init,
         center_learning_rate=args.center_learning_rate,
-        stdev_learning_rate=args.stdev_learning_rate
+        stdev_learning_rate=args.stdev_learning_rate,
     )
 
     if args.interval != 0:
@@ -45,12 +71,12 @@ if __name__ == '__main__':
 
     searcher.run(args.episodes)
     logger.to_dataframe().mean_eval.plot()
-    plt.title(f'{args.save_path}, {args.episodes} episodes')
-    plt.xlabel('episode')
-    plt.ylabel('reward')
+    plt.title(f"{args.save_path}, {args.episodes} episodes")
+    plt.xlabel("episode")
+    plt.ylabel("reward")
     plt.legend()
     plt.grid()
     plt.show()
 
-    trained_network = problem.parameterize_net(searcher.status["best"])
+    trained_network = problem.parameterize_net(searcher.status["center"])
     torch.save(trained_network, args.save_path)
